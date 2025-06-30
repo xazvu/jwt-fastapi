6433341f843bebd912a0b91fb58d2112f93ffa32
Ahmedik <isa732827@gmail.com>
2025-07-01 02:39:04 +0300

    Исправил ошибку в ручке на post так же исправил ошибку в .env

--- auth.py
+++ auth.py
@@ -10,6 +10,9 @@ from models import Users
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 
 router = APIRouter(
@@ -51,8 +54,8 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     db.add(create_user_model)
     db.commit()
 
-@router.get("/token", response_model=Token)
-async def login_for_access_toke(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
                                 db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
