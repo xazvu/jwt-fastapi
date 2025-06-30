15ea7df16495ce4f3f9d60071a41735f916fcb34
Ahmedik <isa732827@gmail.com>
2025-07-01 02:53:13 +0300

    Завершил авторизацию все работает корректно

--- auth.py
+++ auth.py
@@ -81,3 +81,17 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     expires = datetime.utcnow() + expires_delta
     encode.update({'exp': expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str,Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise HTTPException(status.HTTP_401_UNAUTHORIZED,
+                                detail="Could not validate user")
+        return {'username': username, 'id': user_id}
+    except JWTError:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED,
+                            detail="Could not validate user")
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import uvicorn
+from sqlalchemy.testing.pickleable import User
+
 import models
 from fastapi import FastAPI, status, Depends, HTTPException
 from database import init_db, delete_db
 from typing import Annotated
 from sqlalchemy.orm import Session
-from auth import get_db, router
+from auth import get_db, router, get_current_user
 
 init_db()
 
@@ -14,10 +16,10 @@ app = FastAPI()
 app.include_router(router)
 
 db_dependency = Annotated[Session, Depends(get_db)]
-
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 @app.get("/", status_code=status.HTTP_200_OK)
-async def user(user: None, db: db_dependency):
+async def user(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail="Authentication Failed")
     return {'User': user}
